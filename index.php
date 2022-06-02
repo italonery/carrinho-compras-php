e7e26f19222b2bb6d6a2cc52e34c5bc242410d7a
Ítalo Nery <89074342+italonery@users.noreply.github.com>
2022-06-02 19:21:57 -0300

    revisão e correção

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 session_start();
-// Inclusão das funções e conectar ao database usando PDO MySQL
 include 'funcoes.php';
 $pdo = pdo_connect_mysql();
-// A página (home.php) é setada como página inicial padrão, portanto quando essa será a página que o visitante verá.
 $page = isset($_GET['page']) && file_exists($_GET['page'] . '.php') ? $_GET['page'] : 'home';
-// Incluir e mostrar a página solicitada
 include $page . '.php';
 ?>
